2e8fdd496ae7bca6f69f12b1c0af7d22097394ad
VarinderSH <varindersh@hsphoenix.com>
2025-02-04 11:46:31 +0530

    update

--- HSPAnalytics/HSPAnalytics/Classes/APIClient.swift
+++ HSPAnalytics/HSPAnalytics/Classes/APIClient.swift
@@ -11,9 +11,7 @@ import Foundation
 
 class APIClient {
     static let shared = APIClient()
-    
-    private let baseURL = "https://hsp-analytic-kwdph.ondigitalocean.app/hsp-analytic-v2/hsp-event"
-    
+        
     private init() {}
     
     func sendEvent(event: EventRequest, completion: @escaping (Bool) -> Void) {
